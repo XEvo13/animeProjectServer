cfd4c4bd275ace9ce252612b3e34e700e2327163
Francisco Soares <franciscosoares13@hotmail.com>
2024-05-31 10:55:04 +0100

    half comment

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,28 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
 
+// GET USER
+router.get("/:userId", (req, res)=> {
+    const {userId} = req.params;
+
+    User.findById(userId)
+    .populate("friends")
+    .populate("animes")
+    .populate("comments")
+    .populate("ratings")
+    .then((user)=> {
+        if(!user){
+            return res.status(404).json({error: 'User not found' })
+        }
+        res.status(200).json(user)
+    })
+    .catch((error)=> {
+        console.error('Error getting user by Id', error);
+        res.status(500).json({ error: 'Failed to get user by Id'});
+    })
+})
+
+
 // USER EDIT
 
 router.put("/:userId/edit", (req, res)=>{
