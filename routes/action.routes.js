f70e8b291ec30bbae5e4af6d2f5437eb753c9721
Francisco Soares <franciscosoares13@hotmail.com>
2024-06-03 10:29:08 +0100

    addded friends actions

--- routes/action.routes.js
+++ routes/action.routes.js
@@ -3,17 +3,48 @@ const router = express.Router();
 const Action = require("../models/Action.model");
 
 // Create a new action
-router.post("/actions", (req, res) => {
-  const { user, anime, type, comment, rating } = req.body;
-  const newAction = new Action({ user, type, comment, rating, anime });
+// router.post("/actions", (req, res) => {
+//   const { user, anime, type, comment, rating } = req.body;
+//   const newAction = new Action({ user, type, comment, rating, anime });
 
-  newAction
-    .save()
-    .then((savedAction) => {
-      res.status(201).json(savedAction);
+//   newAction
+//     .save()
+//     .then((savedAction) => {
+//       res.status(201).json(savedAction);
+//     })
+//     .catch((error) => {
+//       res.status(400).json({ message: "Error creating action", error });
+//     });
+// });
+
+// GET  
+
+router.get("/:userId/friendsactions", (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .populate("friends")
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      const friendsIds = user.friends.map(friend => friend._id);
+
+      return Action.find({ user: { $in: friendsIds } })
+        .populate("user")
+        .populate("anime")
+        .populate("comment")
+        .populate("rating")
+        .sort({ createdAt: -1 })
+        .limit(2)
+        .then(actions => {
+          res.status(200).json(actions);
+        });
     })
-    .catch((error) => {
-      res.status(400).json({ message: "Error creating action", error });
+    .catch(error => {
+      console.error("Error fetching friends' actions", error);
+      res.status(500).json({ error: "Failed to fetch friends' actions" });
     });
 });
 
